1ec2c4cdb4d5abbddf7baaebcac214d4720f673d
mohamedmostaffa <mohammedmosttaafaa@gmail.com>
2025-07-25 18:32:34 +0300

    Add full unit tests for Kelvin and Fahrenheit conversions

--- test_converter.py
+++ test_converter.py
@@ -7,6 +7,17 @@ class TestConverter(unittest.TestCase):
 
     def test_f_to_c(self):
         self.assertAlmostEqual(fahrenheit_to_celsius(32), 0)
+    def test_c_to_k(self):
+        self.assertAlmostEqual(celsius_to_kelvin(0), 273.15)
+
+    def test_k_to_c(self):
+        self.assertAlmostEqual(kelvin_to_celsius(273.15), 0)
+
+    def test_f_to_k(self):
+        self.assertAlmostEqual(fahrenheit_to_kelvin(32), 273.15)
+
+    def test_k_to_f(self):
+        self.assertAlmostEqual(kelvin_to_fahrenheit(273.15), 32)
 
 if __name__ == '__main__':
     unittest.main()
